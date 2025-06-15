2911b71e4d5d7244b8e13c952325f234424bcce7
Danny-dev23 <petrovslavik1978@gmail.com>
2025-06-15 22:03:35 +0600

    Updated Product.js

--- src/Assets/Product/Product.js
+++ src/Assets/Product/Product.js
@@ -697,7 +697,13 @@ const staron = [
 
 const grandex = [
   {
-    
+        id: 77,
+    name: "Ноутбук",
+    color: "Igneous FI187",
+    category: "Tempest",
+     price: "330$",
+    image:
+      "https://territoriyakomforta.kg"
   }
 ]
  
